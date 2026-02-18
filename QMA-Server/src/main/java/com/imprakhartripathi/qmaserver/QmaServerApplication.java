c93bf4be8a5773207ea7a4c55501aa32dc3971ef
Prakhar Tripathi <imprakhartripathiofficial@gmail.com>
2026-02-18 15:49:38 +0530

    project init

---
+++ QMA-Server/src/main/java/com/imprakhartripathi/qmaserver/QmaServerApplication.java
@@ -0,0 +1,13 @@
+package com.imprakhartripathi.qmaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QmaServerApplication.class, args);
+    }
+
+}
---
+++ QMA-Server/src/test/java/com/imprakhartripathi/qmaserver/QmaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imprakhartripathi.qmaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
